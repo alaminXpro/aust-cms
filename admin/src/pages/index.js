0f967aa63df8538d19ae125bffe311a51479907a
alaminxpro <alaminxpro@gmail.com>
2024-05-31 03:02:06 +0600

    Default AUST CMS
    Signed-off-by: alaminxpro <alaminxpro@gmail.com>

---
+++ admin/src/pages/index.js
@@ -0,0 +1,13 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <h1 className = "text-4xl font-bold bg-center">Welcome to the Admin Dashboard</h1>
+    </main>
+  );
+}
