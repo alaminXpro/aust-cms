ca3df80206b330ae649128ad48de4b66fcd69b87
HariqueJaif <harique02jaif@gmail.com>
2024-06-05 01:41:49 +0600

    Club logos

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,7 @@
 import Hero19 from "@/components/sections/Hero19"
 import Layout from "../components/layout/Layout"
+import Hs1 from "@/components/sections/hs1"
+import Hs2 from "@/components/sections/hs2"
 
 export default function Home() {
 
@@ -7,6 +9,11 @@ export default function Home() {
         <>
             <Layout headerStyle={1} footerStyle={3} headerCls="navbar-dark light-hero-header">
                 <Hero19 />
+                <hr className="divider" />
+                <Hs1/>
+                <hr className="divider" />
+                <Hs2/>
+
                 
             </Layout>
         </>
