dd0c262176af1d0484337560391d1af1886591b2
alaminxpro <alaminxpro@gmail.com>
2024-06-27 00:10:19 +0600

    Server

--- server/server.js
+++ server/server.js
@@ -45,15 +45,15 @@ app.use(log);
 // });
 
 // Error handling middleware
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
-  return res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
-  });
-});
+// app.use((err, req, res, next) => {
+//   const statusCode = err.statusCode || 500;
+//   const message = err.message || 'Internal Server Error';
+//   return res.status(statusCode).json({
+//     success: false,
+//     statusCode,
+//     message,
+//   });
+// });
 
 
 export default app;
