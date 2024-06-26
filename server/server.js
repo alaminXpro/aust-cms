f176617d1cde94067ae0e0fcf1dbdd7f51ae44b8
alaminxpro <alaminxpro@gmail.com>
2024-06-26 19:15:15 +0600

    Server fixed

--- server/server.js
+++ server/server.js
@@ -1,41 +1,50 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-//import userRouter from './routes/user.route.js';
-//import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
-//import path from 'path';
+import path from 'path';
+import cors from 'cors';
+import log from "./middlewares/logger.js";
+
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
-.then(() => {
-    console.log("Connected to database");
-    app.listen(3000, ()=> {
-      console.log("Server is running on http://localhost:3000");
-  });
-})
-.catch((err) => {
-    console.log("Connection failed with error: " + err);
-});
-
+const app = express();
 const __dirname = path.resolve();
 
-const app = express();
-
+// Middleware
 app.use(express.json());
-
 app.use(cookieParser());
+app.use(cors());
 
-//app.use('/api/user', userRouter);
-//app.use('/api/auth', authRouter);
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to database");
+    app.listen(process.env.PORT, () => {
+      console.log("Server is running on http://localhost:" + process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("Connection failed with error: " + err);
+  });
 
-
+// Serve static files
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
+// Routes
+app.get('/test', (req, res) => {
+  res.send('API is running');
+});
+
+// API logger
+app.use(log);
+
+// Default route
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'def', 'index.html'));
-})
+});
 
+// Error handling middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
@@ -45,3 +54,6 @@ app.use((err, req, res, next) => {
     message,
   });
 });
+
+
+export default app;
