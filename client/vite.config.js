b7da3593eb3b13b39f692d70891fbaab0978adbc
alaminxpro <alaminxpro@gmail.com>
2024-06-28 17:00:12 +0600

    API Base

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-      '/api': {
-        target: 'https://aust-cms-server.vercel.app/',
-        secure: false,
-      },
-    },
+    
   },
   plugins: [react()],
 });
