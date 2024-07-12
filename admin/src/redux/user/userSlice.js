08f7c6f583e270ac541c9cb9dd1f726bd2c77362
alaminxpro <alaminxpro@gmail.com>
2024-07-12 22:36:07 +0600

    Redux state modified

--- admin/src/redux/user/userSlice.js
+++ admin/src/redux/user/userSlice.js
@@ -22,6 +22,18 @@ const userSlice = createSlice({
       state.error = action.payload;
       state.loading = false;
     },
+    signUpStart: (state) => {
+      state.loading = true;
+    },
+    signUpSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    signUpFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
     updateUserStart: (state) => {
       state.loading = true;
     },
@@ -65,6 +77,9 @@ export const {
   signInStart,
   signInSuccess,
   signInFailure,
+  signUpStart,
+  signUpSuccess,
+  signUpFailure,
   updateUserFailure,
   updateUserSuccess,
   updateUserStart,
